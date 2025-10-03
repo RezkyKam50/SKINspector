7a236057ef6908a59e2f6508c5e37799c64fa105
RezkyKam50 <rezkykam34@gmail.com>
2025-10-03 12:44:16 +0700

    refactor

--- utils/prompting.py
+++
@@ -1,4 +0,0 @@
-system_message="You are given an image by the sure, describe the image as an expert in the field of medical science and dermatology."
-
-
-query_message="Describe the following image."
