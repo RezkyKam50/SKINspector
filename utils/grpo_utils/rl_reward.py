73ae29aeda61b670d66748ee2647ea8505e78a05
RezkyKam50 <rezkykam34@gmail.com>
2026-01-06 18:36:34 +0700

    reinforcement learning w/ GRPO

--- utils/grpo_utils/rl_reward.py
+++ utils/grpo_utils/rl_reward.py
@@ -6,6 +6,7 @@ for penalizing functions ranges from -1.0 to 1.0 in floating points.
 '''
 
 def format_reward(completions, **kwargs):
+    '''Binary reward function for thinking mode.'''
     pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>$"
     matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completions]
     rewards = [1.0 if match else 0.0 for match in matches]
@@ -19,7 +20,7 @@ def telederm_reward(completions, solution, **kwargs):
     :param solution: Description
     :param kwargs: Description
     '''
-    
+
     return None
     
 
